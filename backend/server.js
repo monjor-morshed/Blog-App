d29dfe66faa4331dee1b6ce2322ea48e524430e9
monjor-morshed <2020-1-60-227@std.ewubd.edu>
2024-05-18 13:23:12 +0600

    signup page added and some api

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,34 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import userRoutes from "./routes/userRoute.js";
+import authRoutes from "./routes/authRoute.js";
 dotenv.config();
+
+const app = express();
+app.use(express.json());
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("Connected to database");
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
   })
   .catch(() => {
     console.log("Connection failed");
   });
 
-const app = express();
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => {
-  console.log("Server is running on 3000");
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,59 @@
 // import React from 'react'
+import { Label, TextInput, Button } from "flowbite-react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left side */}
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Blog
+            </span>
+            App
+          </Link>
+          <p className="text-sm mt-5">
+            This is a blog app where you can share your thoughts and ideas with
+            the world. Sign up now to start sharing. 🚀
+          </p>
+        </div>
+        {/* right side */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="Your Username" />
+              <TextInput type="text" placeHolder="Username" id="username" />
+            </div>
+            <div>
+              <Label value="Your Email" />
+              <TextInput
+                type="email"
+                placeHolder="name@domain.com"
+                id="email"
+              />
+            </div>
+            <div>
+              <Label value="Your Password" />
+              <TextInput type="password" placeHolder="Password" id="password" />
+            </div>
+            <Button gradientDuoTone="purpleToPink" type="submit">
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>
+              Already have an account?{" "}
+              <Link to="/sign-in" className="text-blue-500">
+                Sign In
+              </Link>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
